5e410c3a9c19a9e97660c5768a02364d93565a7c
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2015-09-17 12:36:23 -0300

    Square code style and Travis CI
    Feel happy to analyze my code, I think it`s a better code style for
    Android

--- app/src/main/java/co/mobiwise/progresslayout/RecylerListAdapter.java
+++ app/src/main/java/co/mobiwise/progresslayout/RecylerListAdapter.java
@@ -29,195 +29,157 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import co.mobiwise.library.ProgressLayout;
 
-/**
- * Created by mertsimsek on 16/09/15.
- */
-public class RecylerListAdapter extends RecyclerView.Adapter<RecylerListAdapter.ViewHolder>{
+public class RecylerListAdapter extends RecyclerView.Adapter<RecylerListAdapter.ViewHolder> {
 
-    /**
-     * Track list
-     */
-    private List<Track> trackList;
+  /**
+   * Track list
+   */
+  private List<Track> trackList;
 
-    /**
-     * need to keep current playing.
-     */
-    private Track currentTrack;
+  /**
+   * need to keep current playing.
+   */
+  private Track currentTrack;
 
-    /**
-     * Current duration
-     */
-    private int currentDuration = 0;
+  /**
+   * Current duration
+   */
+  private int currentDuration = 0;
 
-    /**
-     * is currently playing
-     */
-    private boolean isPlaying = false;
+  /**
+   * is currently playing
+   */
+  private boolean isPlaying = false;
 
-    private static final int SECOND_MS = 1000;
+  private static final int SECOND_MS = 1000;
 
-    /**
-     * needed handler for recyclerview
-     */
-    private Handler mHandler;
+  /**
+   * needed handler for recyclerview
+   */
+  private Handler mHandler = new Handler();
 
-    /**
-     * count up second
-     */
-    private Runnable mRunnable;
+  /**
+   * count up second
+   */
+  private final Runnable mRunnable = new Runnable() {
+    @Override public void run() {
+      currentDuration += 1;
+      mHandler.postDelayed(mRunnable, SECOND_MS);
+    }
+  };
 
-    /**
-     * Constructor
-     */
-    public RecylerListAdapter() {
+  /**
+   * set track list
+   */
+  public void setTrackList(List<Track> trackList) {
+    this.trackList = trackList;
+    notifyDataSetChanged();
+  }
 
-        mHandler = new Handler();
+  /**
+   * Create View holder
+   */
+  @Override public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    View view =
+        LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+    ViewHolder viewHolder = new ViewHolder(view);
+    return viewHolder;
+  }
 
-        mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                currentDuration += 1;
-                mHandler.postDelayed(mRunnable, SECOND_MS);
-            }
-        };
+  /**
+   * I did little bit trick on this method
+   * Normally ProgressLayout library already has runnable to update
+   * current second and current progress. But with ViewHolder pattern
+   * when we scroll down or up, viewholder objects which we created before
+   * are using again. That is why i keep some data to check whether item is playing
+   * or not.
+   */
+  @Override public void onBindViewHolder(final ViewHolder viewHolder, final int i) {
+
+    final Track track = trackList.get(i);
+
+    viewHolder.textViewDuration.setText(calculateSongDuration(track.getDurationInSec()));
+    viewHolder.textViewSong.setText(track.getSongName());
+    viewHolder.textViewSinger.setText(track.getSingerName());
+    viewHolder.imageViewAction.setBackgroundResource(R.drawable.play);
+    viewHolder.progressLayout.setMaxProgress(track.getDurationInSec());
+
+    if (currentTrack != null && currentTrack == track) {
+      viewHolder.imageViewAction.setBackgroundResource(
+          isPlaying ? R.drawable.pause : R.drawable.play);
+      viewHolder.progressLayout.setCurrentProgress(currentDuration);
+      if (isPlaying) viewHolder.progressLayout.start();
+    } else {
+      viewHolder.progressLayout.cancel();
     }
 
-    /**
-     * set track list
-     * @param trackList
-     */
-    public void setTrackList(List<Track> trackList){
-        this.trackList = trackList;
-        notifyDataSetChanged();
-    }
+    viewHolder.imageViewAction.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
 
-    /**
-     * Create View holder
-     * @param viewGroup
-     * @param i
-     * @return
-     */
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
-    }
+        if (track != currentTrack) {
+          currentTrack = track;
+          mHandler.removeCallbacks(mRunnable);
+          currentDuration = 0;
+        }
 
-    /**
-     * I did little bit trick on this method
-     * Normally ProgressLayout library already has runnable to update
-     * current second and current progress. But with ViewHolder pattern
-     * when we scroll down or up, viewholder objects which we created before
-     * are using again. That is why i keep some data to check whether item is playing
-     * or not.
-     * @param viewHolder
-     * @param i
-     */
-    @Override
-    public void onBindViewHolder(final ViewHolder viewHolder, final int i) {
-
-        final Track track = trackList.get(i);
-
-        viewHolder.textViewDuration.setText(calculateSongDuration(track.getDurationInSec()));
-        viewHolder.textViewSong.setText(track.getSongName());
-        viewHolder.textViewSinger.setText(track.getSingerName());
+        if (!viewHolder.progressLayout.isPlaying()) {
+          isPlaying = true;
+          viewHolder.progressLayout.start();
+          mHandler.postDelayed(mRunnable, 0);
+          viewHolder.imageViewAction.setBackgroundResource(R.drawable.pause);
+          notifyDataSetChanged();
+        } else {
+          isPlaying = false;
+          viewHolder.progressLayout.stop();
+          mHandler.removeCallbacks(mRunnable);
+          viewHolder.imageViewAction.setBackgroundResource(R.drawable.play);
+          notifyDataSetChanged();
+        }
+      }
+    });
+    viewHolder.progressLayout.setProgressLayoutListener(new ProgressLayoutListener() {
+      @Override public void onProgressCompleted() {
         viewHolder.imageViewAction.setBackgroundResource(R.drawable.play);
-        viewHolder.progressLayout.setMaxProgress(track.getDurationInSec());
+      }
 
-        if(currentTrack != null && currentTrack == track){
-            viewHolder.imageViewAction.setBackgroundResource(isPlaying ? R.drawable.pause : R.drawable.play);
-            viewHolder.progressLayout.setCurrentProgress(currentDuration);
-            if(isPlaying)
-                viewHolder.progressLayout.start();
-        }
-        else {
-            viewHolder.progressLayout.cancel();
-        }
+      @Override public void onProgressChanged(int seconds) {
+        viewHolder.textViewDuration.setText(calculateSongDuration(seconds));
+      }
+    });
+  }
 
-        viewHolder.imageViewAction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+  /**
+   * List count
+   */
+  @Override public int getItemCount() {
+    return trackList.size();
+  }
 
-                if(track != currentTrack){
-                    currentTrack = track;
-                    mHandler.removeCallbacks(mRunnable);
-                    currentDuration = 0;
-                }
+  /**
+   * Calculate in string (hh:mm)
+   */
+  private String calculateSongDuration(int seconds) {
+    return new StringBuilder(String.valueOf(seconds / 60))
+        .append(":")
+        .append(String.valueOf(seconds % 60))
+        .toString();
+  }
 
-                if (!viewHolder.progressLayout.isPlaying()) {
-                    isPlaying = true;
-                    viewHolder.progressLayout.start();
-                    mHandler.postDelayed(mRunnable,0);
-                    viewHolder.imageViewAction.setBackgroundResource(R.drawable.pause);
-                    notifyDataSetChanged();
+  /**
+   * ViewHolder
+   */
+  public static class ViewHolder extends RecyclerView.ViewHolder {
 
-                } else {
-                    isPlaying = false;
-                    viewHolder.progressLayout.stop();
-                    mHandler.removeCallbacks(mRunnable);
-                    viewHolder.imageViewAction.setBackgroundResource(R.drawable.play);
-                    notifyDataSetChanged();
-                }
-            }
-        });
-        viewHolder.progressLayout.setProgressLayoutListener(new ProgressLayoutListener() {
-            @Override
-            public void onProgressCompleted() {
-                viewHolder.imageViewAction.setBackgroundResource(R.drawable.play);
-            }
+    @Bind(R.id.imageviewAction) ImageView imageViewAction;
+    @Bind(R.id.progressLayout) ProgressLayout progressLayout;
+    @Bind(R.id.textviewSong) TextView textViewSong;
+    @Bind(R.id.textviewSinger) TextView textViewSinger;
+    @Bind(R.id.textviewDuration) TextView textViewDuration;
 
-            @Override
-            public void onProgressChanged(int seconds) {
-                viewHolder.textViewDuration.setText(calculateSongDuration(seconds));
-            }
-        });
-
+    public ViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
     }
 
-    /**
-     * List count
-     * @return
-     */
-    @Override
-    public int getItemCount() {
-        return trackList.size();
-    }
-
-    /**
-     * Calculate in string (hh:mm)
-     * @param seconds
-     * @return
-     */
-    private String calculateSongDuration(int seconds){
-        String minutesText = String.valueOf(seconds / 60);
-        String secondText = String.valueOf(seconds % 60);
-        return minutesText + ":" + secondText;
-    }
-
-    /**
-     * ViewHolder
-     */
-    static class ViewHolder extends RecyclerView.ViewHolder{
-
-        @Bind(R.id.imageviewAction)
-        ImageView imageViewAction;
-
-        @Bind(R.id.progressLayout)
-        ProgressLayout progressLayout;
-
-        @Bind(R.id.textviewSong)
-        TextView textViewSong;
-
-        @Bind(R.id.textviewSinger)
-        TextView textViewSinger;
-
-        @Bind(R.id.textviewDuration)
-        TextView textViewDuration;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-        }
-    }
+  }
 }
--- library/src/main/java/co/mobiwise/library/ProgressLayout.java
+++ library/src/main/java/co/mobiwise/library/ProgressLayout.java
@@ -28,152 +28,150 @@ import android.view.View;
 
 public class ProgressLayout extends View implements Animatable {
 
-    private static final int COLOR_EMPTY_DEFAULT = 0x00000000;
-    private static final int COLOR_LOADED_DEFAULT = 0x11FFFFFF;
+  private static final int COLOR_EMPTY_DEFAULT = 0x00000000;
+  private static final int COLOR_LOADED_DEFAULT = 0x11FFFFFF;
+  private static final int PROGRESS_SECOND_MS = 1000;
 
-    private static int PROGRESS_SECOND_MS = 1000;
+  private static Paint paintProgressLoaded;
+  private static Paint paintProgressEmpty;
 
-    private static Paint paintProgressLoaded;
-    private static Paint paintProgressEmpty;
+  private boolean isPlaying = false;
+  private boolean isAutoProgress;
 
-    private boolean isPlaying = false;
-    private boolean isAutoProgress;
+  private int mHeight;
+  private int mWidth;
+  private int maxProgress;
+  private int currentProgress = 0;
 
-    private int mHeight;
-    private int mWidth;
-    private int maxProgress;
-    private int currentProgress = 0;
+  private Handler handlerProgress;
 
-    private Handler handlerProgress;
+  private ProgressLayoutListener progressLayoutListener;
 
-    private ProgressLayoutListener progressLayoutListener;
+  public ProgressLayout(Context context) {
+    this(context, null);
+  }
 
-    public ProgressLayout(Context context) {
-        this(context, null);
+  public ProgressLayout(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public ProgressLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init(context, attrs);
+  }
+
+  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+  public ProgressLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    super(context, attrs, defStyleAttr, defStyleRes);
+    init(context, attrs);
+  }
+
+  @Override public boolean isRunning() {
+    return isPlaying;
+  }
+
+  @Override public void start() {
+    if (isAutoProgress) {
+      isPlaying = true;
+      handlerProgress.removeCallbacksAndMessages(null);
+      handlerProgress.postDelayed(mRunnableProgress, 0);
     }
+  }
 
-    public ProgressLayout(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+  @Override public void stop() {
+    isPlaying = false;
+    handlerProgress.removeCallbacks(mRunnableProgress);
+    postInvalidate();
+  }
 
-    public ProgressLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context, attrs);
-    }
+  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    mWidth = MeasureSpec.getSize(widthMeasureSpec);
+    mHeight = MeasureSpec.getSize(heightMeasureSpec);
+  }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public ProgressLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs);
-    }
+  @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    canvas.drawRect(0, 0, mWidth, mHeight, paintProgressEmpty);
+    canvas.drawRect(0, 0, calculatePositionIndex(currentProgress), mHeight, paintProgressLoaded);
+  }
 
-    @Override public boolean isRunning() {
-        return isPlaying;
-    }
+  private void init(Context context, AttributeSet attrs) {
+    setWillNotDraw(false);
+    TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.progressLayout);
+    isAutoProgress = a.getBoolean(R.styleable.progressLayout_autoProgress, true);
+    maxProgress = a.getInt(R.styleable.progressLayout_maxProgress, 0);
+    maxProgress = maxProgress * 10;
+    int loadedColor = a.getColor(R.styleable.progressLayout_loadedColor, COLOR_LOADED_DEFAULT);
+    int emptyColor = a.getColor(R.styleable.progressLayout_emptyColor, COLOR_EMPTY_DEFAULT);
+    a.recycle();
 
-    @Override public void start() {
-        if(isAutoProgress){
-            isPlaying = true;
-            handlerProgress.removeCallbacksAndMessages(null);
-            handlerProgress.postDelayed(mRunnableProgress, 0);
+    paintProgressEmpty = new Paint();
+    paintProgressEmpty.setColor(emptyColor);
+    paintProgressEmpty.setStyle(Paint.Style.FILL);
+    paintProgressEmpty.setAntiAlias(true);
+
+    paintProgressLoaded = new Paint();
+    paintProgressLoaded.setColor(loadedColor);
+    paintProgressLoaded.setStyle(Paint.Style.FILL);
+    paintProgressLoaded.setAntiAlias(true);
+
+    handlerProgress = new Handler();
+  }
+
+  private int calculatePositionIndex(int currentProgress) {
+    return (currentProgress * mWidth) / maxProgress;
+  }
+
+  public boolean isPlaying() {
+    return isPlaying;
+  }
+
+  public void cancel() {
+    isPlaying = false;
+    currentProgress = 0;
+    handlerProgress.removeCallbacks(mRunnableProgress);
+    postInvalidate();
+  }
+
+  public void setCurrentProgress(int currentProgress) {
+    this.currentProgress = currentProgress * 10;
+    postInvalidate();
+  }
+
+  public void setMaxProgress(int maxProgress) {
+    this.maxProgress = maxProgress * 10;
+    postInvalidate();
+  }
+
+  public void setAutoProgress(boolean isAutoProgress) {
+    this.isAutoProgress = isAutoProgress;
+  }
+
+  public void setProgressLayoutListener(ProgressLayoutListener progressLayoutListener) {
+    this.progressLayoutListener = progressLayoutListener;
+  }
+
+  private final Runnable mRunnableProgress = new Runnable() {
+    @Override public void run() {
+      if (isPlaying) {
+        if (currentProgress == maxProgress) {
+          if (progressLayoutListener != null) {
+            progressLayoutListener.onProgressCompleted();
+          }
+          currentProgress = 0;
+          setCurrentProgress(currentProgress);
+          stop();
+        } else {
+          postInvalidate();
+          currentProgress += 1;
+          if (progressLayoutListener != null) {
+            progressLayoutListener.onProgressChanged(currentProgress / 10);
+          }
+          handlerProgress.postDelayed(mRunnableProgress, PROGRESS_SECOND_MS / 10);
         }
+      }
     }
-
-    @Override public void stop(){
-        isPlaying = false;
-        handlerProgress.removeCallbacks(mRunnableProgress);
-        postInvalidate();
-    }
-
-    private void init(Context context, AttributeSet attrs) {
-        setWillNotDraw(false);
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.progressLayout);
-        isAutoProgress = a.getBoolean(R.styleable.progressLayout_autoProgress, true);
-        maxProgress = a.getInt(R.styleable.progressLayout_maxProgress, 0);
-        maxProgress = maxProgress * 10;
-        int loadedColor = a.getColor(R.styleable.progressLayout_loadedColor, COLOR_LOADED_DEFAULT);
-        int emptyColor = a.getColor(R.styleable.progressLayout_emptyColor, COLOR_EMPTY_DEFAULT);
-        a.recycle();
-
-        paintProgressEmpty = new Paint();
-        paintProgressEmpty.setColor(emptyColor);
-        paintProgressEmpty.setStyle(Paint.Style.FILL);
-        paintProgressEmpty.setAntiAlias(true);
-
-        paintProgressLoaded = new Paint();
-        paintProgressLoaded.setColor(loadedColor);
-        paintProgressLoaded.setStyle(Paint.Style.FILL);
-        paintProgressLoaded.setAntiAlias(true);
-
-        handlerProgress = new Handler();
-    }
-
-    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        mWidth = MeasureSpec.getSize(widthMeasureSpec);
-        mHeight = MeasureSpec.getSize(heightMeasureSpec);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawRect(0, 0, mWidth, mHeight, paintProgressEmpty);
-        canvas.drawRect(0, 0, calculatePositionIndex(currentProgress), mHeight, paintProgressLoaded);
-    }
-
-    private int calculatePositionIndex(int currentProgress){
-        return (currentProgress * mWidth) / maxProgress;
-    }
-
-    public boolean isPlaying(){
-        return isPlaying;
-    }
-
-    public void cancel(){
-        isPlaying = false;
-        currentProgress = 0;
-        handlerProgress.removeCallbacks(mRunnableProgress);
-        postInvalidate();
-    }
-
-    public void setCurrentProgress(int currentProgress){
-        this.currentProgress = currentProgress * 10;
-        postInvalidate();
-    }
-
-    public void setMaxProgress(int maxProgress){
-        this.maxProgress = maxProgress * 10;
-        postInvalidate();
-    }
-
-    public void setAutoProgress(boolean isAutoProgress){
-        this.isAutoProgress = isAutoProgress;
-    }
-
-    public void setProgressLayoutListener(ProgressLayoutListener progressLayoutListener){
-        this.progressLayoutListener = progressLayoutListener;
-    }
-
-    private final Runnable mRunnableProgress = new Runnable() {
-        @Override public void run() {
-            if (isPlaying) {
-                if(currentProgress == maxProgress){
-                    if(progressLayoutListener != null) {
-                        progressLayoutListener.onProgressCompleted();
-                    }
-                    currentProgress = 0;
-                    setCurrentProgress(currentProgress);
-                    stop();
-                } else {
-                    postInvalidate();
-                    currentProgress += 1;
-                    if(progressLayoutListener != null) {
-                        progressLayoutListener.onProgressChanged(currentProgress / 10);
-                    }
-                    handlerProgress.postDelayed(mRunnableProgress, PROGRESS_SECOND_MS / 10);
-                }
-            }
-        }
-    };
+  };
 
 }
