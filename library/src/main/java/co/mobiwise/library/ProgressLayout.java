10e2c0e963ca9db852eb21b10d0880c501e81219
william Wilbur <wilbersoft@yahoo.com>
2017-05-16 18:38:30 +0100

    Added javadocs and setters & getters

--- library/src/main/java/co/mobiwise/library/ProgressLayout.java
+++ library/src/main/java/co/mobiwise/library/ProgressLayout.java
@@ -25,7 +25,6 @@ import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 public class ProgressLayout extends View implements Animatable {
